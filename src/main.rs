b394933e37698a8cc7ba78b0dfe4c905c6759f15
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-08-07 07:42:33 -0400

    Updated dependencies.

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,10 @@ pub use novusk;
 #[cfg(target_arch = "x86_64")]
 pub(crate) mod x86_64;
 
-
-pub(crate) mod graphics;
-
 #[no_mangle]
-pub unsafe extern "C" fn kernel_main() -> ! {
+pub extern "C" fn kernel_main() -> ! {
     #[cfg(target_arch = "x86_64")]
     x86_64::x86_64_setup();
 
-    loop {  }
+    panic!("FOMOSv3-Blue v3 finished all processes");
 }
